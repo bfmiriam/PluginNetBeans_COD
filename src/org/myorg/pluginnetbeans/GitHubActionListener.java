4ba9a6b0deac44525910e2057c95145fff839680
mbacelofernandez <mbacelofernandez@victoria12.danielcastelao.org>
2018-04-24 09:46:30 +0200

    ActionListener creado, crea el repositorio

--- src/org/myorg/pluginnetbeans/GitHubActionListener.java
+++ src/org/myorg/pluginnetbeans/GitHubActionListener.java
@@ -7,10 +7,14 @@ package org.myorg.pluginnetbeans;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.IOException;
 import javax.swing.JOptionPane;
+import org.kohsuke.github.GHCreateRepositoryBuilder;
+import org.kohsuke.github.GitHub;
 import org.openide.awt.ActionID;
 import org.openide.awt.ActionReference;
 import org.openide.awt.ActionRegistration;
+import org.openide.util.Exceptions;
 import org.openide.util.NbBundle.Messages;
 
 @ActionID(
@@ -27,6 +31,15 @@ public final class GitHubActionListener implements ActionListener {
 
     @Override
     public void actionPerformed(ActionEvent e) {
-
+        try {
+            String repo = JOptionPane.showInputDialog("Indique el nombre para el repositorio");
+            
+            GitHub github = GitHub.connect();
+            GHCreateRepositoryBuilder builder;
+            builder = github.createRepository(repo);
+            builder.create();
+        } catch (IOException ex) {
+            Exceptions.printStackTrace(ex);
+        }
     }
 }
